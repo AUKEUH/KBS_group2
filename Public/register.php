69b7587da6c764705c847d23935ec259068af7ad
AUKEUH <aukeboogaart@casema.nl>
2021-12-02 16:03:50 +0100

    startup register.php

---
+++ Public/register.php
@@ -0,0 +1,28 @@
+<?php
+include "header.php";
+?>
+<div id="CenteredContent">
+    <h1>Regristreren</h1>
+
+    <form method="GET" action="login.php">
+        <label for="username">Emailadres:</label>
+        <input type="text" placeholder="example@email.com" name="username" id="username" required>
+        <br>
+        <label for="password">naam:</label>
+        <input type="text" name="password" id="password" required>
+        <button type="submit" name="submit" class="btn  mt-2 btn-primary">Regristreren</button>
+        <br>
+        <a href="/register" class="HrefDecoration">Ik heb nog geen account</a>
+    </form>
+
+<?php
+if (isset($_GET["submit"])){
+    if ($_GET["username"] === "inkoper" && $_GET["password"] === "spekkoper"){
+        $_SESSION["login"] = TRUE;
+        print "U bent ingelogd";
+        $_SESSION["username"] = $_GET["username"];
+    }else{
+        $_SESSION["login"] = FALSE;
+        print "De inlog gegevens zijn fout";
+    }
+}
