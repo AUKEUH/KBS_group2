f5db27956a4915a70acd483ad70dd10df2f52d09
jochemmars <s1166535@student.windesheim.nl>
2021-12-21 14:14:30 +0100

    inloggen aangepast zodat het werkt met de database

--- login.php
+++ login.php
@@ -1,33 +1,41 @@
 <?php
 include "header.php";
 ?>
-<div id="CenteredContent">
-    <h1>Inloggen</h1>
+    <div id="CenteredContent">
+        <h1>Inloggen</h1>
 
-    <form method="GET" action="login.php">
-        <label for="username">Emailadres:</label>
-        <input type="text" placeholder="example@email.com" name="username" id="username" required>
-        <br>
-        <label for="password">Wachtwoord:</label>
-        <input type="password" name="password" id="password" required>
-        <button type="submit" name="submit" class="btn  mt-2 btn-primary">Log in</button>
-        <a href='login.php?logout=true' class="btn  mt-2 btn-primary">Log out</a>
-        <br>
-        <a href="register.php" class="HrefDecoration">Ik heb nog geen account</a>
-    </form>
+        <form method="GET" action="login.php">
+            <label for="username">Emailadres:</label>
+            <input type="text" placeholder="example@email.com" name="username" id="username" required>
+            <br>
+            <label for="password">Wachtwoord:</label>
+            <input type="password" name="password" id="password" required>
+            <button type="submit" name="submit" class="btn  mt-2 btn-primary">Log in</button>
+            <a href='login.php?logout=true' class="btn  mt-2 btn-primary">Log out</a>
+            <br>
+            <a href="register.php" class="HrefDecoration">Ik heb nog geen account</a>
+        </form>
 
 <?php
-if (isset($_GET["submit"])){
-    if ($_GET["username"] === "inkoper" && $_GET["password"] === "spekkoper"){
-        $_SESSION["login"] = TRUE;
-        print "U bent ingelogd";
-        $_SESSION["username"] = $_GET["username"];
-    }else{
-        $_SESSION["login"] = FALSE;
-        print "De inlog gegevens zijn fout";
+if (isset($_GET['username'] )){
+    $conn = mysqli_connect('localhost', 'root', '', 'nerdygadgets', 3306);
+    $username = $_GET['username'];
+    $password = $_GET['password'];
+    $query = "select * from registratiedata where Emailadres = '$username'";
+    $resultSet = mysqli_query($conn, $query); //Syntax error: mysqli_query(connection,query);
+    if (mysqli_num_rows($resultSet) > 0) {
+        $row = mysqli_fetch_assoc($resultSet);
+        if ($row['Wachtwoord'] == $password) {
+            $_SESSION["login"] = TRUE;
+            print "U bent ingelogd";
+            $_SESSION["username"] = $_GET["username"];
+            $_SESSION["Voornaam"] = $row['Voornaam'];
+        } else {
+            $_SESSION["login"] = FALSE;
+            print "De inlog gegevens zijn fout";
+        }
     }
 }
-
 if (isset($_GET["logout"])){
     $_SESSION["login"] = FALSE;
     $_SESSION["username"] = '';
