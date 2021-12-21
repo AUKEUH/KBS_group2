b4498e710d22b4147f3a255068efecb07eca381b
jochemmars <s1166535@student.windesheim.nl>
2021-12-21 14:07:31 +0100

    insert.php voor registreren

--- ophalendata/insert.php
+++ ophalendata/insert.php
@@ -1,18 +1,23 @@
 <?php
 require_once 'connect.php';
 
-$naam = $_POST["naam"];
-$voorl = $_POST["voorl"];
-$chef = $_POST["chef"];
-$afd = $_POST["afd"];
+$voornaam = $_POST["voornaam"];
+$achternaam = $_POST["achternaam"];
+$geboorte = $_POST["geboorte"];
+$emailadres = $_POST["emailadres"];
+$telefoonnummer = $_POST["telefoonnummer"];
+$straat = $_POST["straat"];
+$huisnummer = $_POST["huisnummer"];
+$postcode = $_POST["postcode"];
+$plaats = $_POST["plaats"];
+$wachtwoord = $_POST["wachtwoord"];
 
+$sql = "INSERT INTO `registratiedata`(`Voornaam`, `Achternaam`, `Geboortedatum`, `Emailadres`, `Telefoonnummer`, `Straat`, `Huisnummer`, `Postcode`, `Plaats`,`Wachtwoord`)
+        VALUES ('$voornaam','$achternaam','$geboorte','$emailadres','$telefoonnummer','$straat','$huisnummer','$postcode','$plaats','$wachtwoord')";
 
-$sql = "INSERT INTO `medewerker`(`naam`, `voorl`, `chef`,`afd`)
-        VALUES ('$naam','$voorl','$chef','$afd')";
+$statement = mysqli_prepare($connection, $sql);
+// mysqli_stmt_bind_param($statement, 'i', $gebruikersinput);
+mysqli_stmt_execute($statement);
+$result = mysqli_stmt_get_result($statement);
 
-  $statement = mysqli_prepare($connection, $sql);
-  // mysqli_stmt_bind_param($statement, 'i', $gebruikersinput);
-  mysqli_stmt_execute($statement);
-  $result = mysqli_stmt_get_result($statement);
-
-  header('Location: ophalendata.php');
+header("Refresh:0; url=http://localhost/git/KBS_group2/login.php");
