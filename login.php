6a17d8599d6af0092b0494ac8207230e51866bd6
jochemmars <s1166535@student.windesheim.nl>
2021-12-23 08:59:53 +0100

    wachtwoord gehashed en inlog buttons gefixt

--- login.php
+++ login.php
@@ -2,19 +2,23 @@
 include "header.php";
 ?>
     <div id="CenteredContent">
-        <h1>Inloggen (2/3)</h1>
+    <h1>Inloggen</h1>
 
-        <form method="GET" action="login.php">
-            <label for="username">Emailadres:</label>
-            <input type="text" placeholder="example@email.com" name="username" id="username" required>
-            <br>
-            <label for="password">Wachtwoord:</label>
-            <input type="password" name="password" id="password" required>
-            <button type="submit" name="submit" class="btn  mt-2 btn-primary">Log in</button>
+    <form method="GET" action="login.php">
+        <label for="username">Emailadres:</label>
+        <input type="text" placeholder="example@email.com" name="username" id="username" required>
+        <br>
+        <label for="password">Wachtwoord:</label>
+        <input type="password" name="password" id="password" required>
+        <?php if($_SESSION["login"] === TRUE) { ?>
             <a href='login.php?logout=true' class="btn  mt-2 btn-primary">Log out</a>
-            <br>
-            <a href="register.php" class="HrefDecoration">Ik heb nog geen account</a>
-        </form>
+        <?php }
+        else { ?>
+            <button type="submit" name="submit" class="btn  mt-2 btn-primary">Log in</button>
+        <?php } ?>
+        <br>
+        <a href="register.php" class="HrefDecoration">Ik heb nog geen account</a>
+    </form>
 
 <?php
 if (isset($_GET['username'] )){
@@ -25,13 +29,13 @@ if (isset($_GET['username'] )){
     $resultSet = mysqli_query($conn, $query); //Syntax error: mysqli_query(connection,query);
     if (mysqli_num_rows($resultSet) > 0) {
         $row = mysqli_fetch_assoc($resultSet);
-        if ($row['Wachtwoord'] == $password) {
+        if (password_verify(hash('sha512', $password), $row['Wachtwoord'])) {
             $_SESSION["login"] = TRUE;
-            print "U bent ingelogd";
             $_SESSION["username"] = $_GET["username"];
             $_SESSION["Voornaam"] = $row['Voornaam'];
-            $_SESSION["RegistratieId"] = $row['RegistratieId'];
-        } else {
+            header("Refresh:0; url=index.php");
+        }
+        else {
             $_SESSION["login"] = FALSE;
             print "De inlog gegevens zijn fout";
         }
