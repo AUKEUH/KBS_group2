2964f555ffa2c3d32ffcbe961901cd5654545a31
Michiel01 <michielalberts16@gmail.com>
2022-01-31 13:25:22 +0100

    review.php aangemaakt

--- review.php
+++ review.php
@@ -2,11 +2,11 @@
 //deze pagina zorgt ervoor dat de reviews in de database worden gestopt
 require_once 'ophalendata/connect.php';
 
-    /* $sql = "INSERT INTO `reviews`(`id`, `page_id`, `name`, `content`, `rating`)
+    $sql = "INSERT INTO `reviews`(`id`, `page_id`, `name`, `content`, `rating`)
           VALUES ($id,$page_id,$name,$content,$rating)";
 
     $statement = mysqli_prepare($connection, $sql);
-    mysqli_stmt_execute($statement); */
+    mysqli_stmt_execute($statement);
 
 //zorgt ervoor dat de rating opgehaald wordt
 $query = "SELECT AVG (rating) FROM reviews";
